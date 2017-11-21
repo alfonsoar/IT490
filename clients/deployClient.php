738e49e6f85129e9d107e54c09117a609f818a5c
aga23 <aga23@njit.edu>
2017-11-21 00:37:58 -0500

    Finished deploy server

--- clients/deployClient.php
+++ clients/deployClient.php
@@ -10,24 +10,39 @@ $user = '';
 $dServDir = '';
 $layer = '';
 
-$string = shell_exec("cat conf.ini | grep -i user | awk '{ print $3 }'");
+$string = shell_exec("cat ../Ini/conf.ini | grep -i user | awk '{ print $3 }'");
 $user = trim(preg_replace('/\s+/', ' ', $string));
-$string1 = shell_exec("cat conf.ini | grep -i deployserver | awk '{ print $3 }'");
+$string1 = shell_exec("cat ../Ini/conf.ini | grep -i deployserver | awk '{ print $3 }'");
 $dServDir = trim(preg_replace('/\s+/', ' ', $string1));
-$string2 = shell_exec("cat conf.ini | grep -i layer | awk '{ print $3 }'");
+$string2 = shell_exec("cat ../Ini/conf.ini | grep -i layer | awk '{ print $3 }'");
 $layer = trim(preg_replace('/\s+/', ' ', $string2));
+$string3 = shell_exec("cat ../Ini/conf.ini | grep -i localmachine | awk '{ print $3 }'");
+$machine = trim(preg_replace('/\s+/', ' ', $string3));
 
 //check to make sure the user eneters a function and Version ID
-if(empty($argv[1]) || empty($argv[2])){
+if(empty($argv[1])){
   echo "Please enter a Valid function".PHP_EOL;
-  echo "Functions: New | Deploy | Rollback".PHP_EOL;
-  echo "Usage: ".$argv[0]." <Function> <Version ID>".PHP_EOL;
+  echo "Functions: New | Deploy | Rollback | Deprecate".PHP_EOL;
+  echo "Usage: ".$argv[0]." <Function>".PHP_EOL;
   exit(0);
 }
 
 $caseD = strtolower($argv[1]);
-$vid = $argv[2];
 
+function checkLtsVer($layer,$machine){
+  $out = sendtoServer($layer,"chkLtsVer","n/a","n/a","n/a",$machine);
+  return $out;
+}
+
+function checkDepC($layer,$machine,$vid){
+  $out = sendtoServer($layer,"chkDep",$vid,"n/a","n/a",$machine);
+  return $out;
+}
+
+function checkDBVer($layer,$machine){
+  $out = sendtoServer($layer,"chkDbVer","n/a","n/a","n/a",$machine);
+  return $out;
+}
 //This function will make a bundle and send it to your approprite folder on
 //the deploy server
 function deployNew($version_id,$user,$dir){
@@ -43,21 +58,43 @@ function deployNew($version_id,$user,$dir){
 
 //this function sends a command to the deploy server
 //this whill deploy an alredy existing bundle
-function deploy($vid,$dip,$dir,$layer){
-  $check = checkVer($vid,$layer);
+function deploy($vid,$dip,$dir,$layer,$machine){
+  $check = checkVer($vid,$layer,$machine);
 
   if($check == 1){
-    sendtoServer($layer,"deployExt",$vid,$dip,$dir);
-
-  }else{
-    echo "The version ".$vid." does not exist in the deploy server".PHP_EOL;
-    exit(0);
+    echo "THE VERSION IM SENDING IF IT's EXITS is: ".$vid.PHP_EOL;
+    $depc = checkDepC($layer,$machine,$vid);
+    if ($depc == 1){
+      echo "THIS version is depecrated fuck off".PHP_EOL;
+    }else{
+      sendtoServer($layer,"deployExt",$vid,$dip,$dir,"n/a");
   }
+  }else{
+    $vid = checkLtsVer($layer,$machine);
+    echo "THE VERSION IM SENDING IF DOES NOT IT's EXITS is: ".$vid.PHP_EOL;
+    $depc = checkDepC($layer,$machine,$vid);
+    if ($depc == 1){
+      echo "THIS version is depecrated fuck off".PHP_EOL;
+    }else{
+      sendtoServer($layer,"deployExt",$vid,$dip,$dir,"n/a");
+    }
+  }
+}
+
+function rollback($vid,$dip,$dir,$layer){
+  echo "THE VERSION is ".$vid.PHP_EOL;
+  sendtoServer($layer,"deployExt",$vid,$dip,$dir,"n/a");
+}
+
+function checkRoll($layer,$machine){
+  $ltsVer = checkLtsVer($layer,$machine);
+  $out = sendtoServer($layer,"chkRoll",$ltsVer,"n/a","n/a",$machine);
+  return $out;
 }
 //check to see if the version alredy enchant_broker_dict_exists
 //this might be taken out if we use a DB to track this
-function checkVer($vid,$layer){
-  $stat = sendtoServer($layer,"chkVer",$vid,"n/a","n/a");
+function checkVer($vid,$layer,$machine){
+  $stat = sendtoServer($layer,"chkVer",$vid,"n/a","n/a",$machine);
   if($stat == 1){
     echo $stat.PHP_EOL;
     return $stat;
@@ -65,10 +102,9 @@ function checkVer($vid,$layer){
   return 0;
 }
 
-
 //send a meesage to the deploy server, make sure the transfer was Successful
 //Also pass the destination IP so the server knows who to send it to
-function sendtoServer($type,$func,$vid,$dip,$dir){
+function sendtoServer($type,$func,$vid,$dip,$dir,$machine){
   $client = new rabbitMQClient("../Ini/deploy.ini","testServer");
   $request = array();
   $request['type'] = $type;
@@ -76,6 +112,7 @@ function sendtoServer($type,$func,$vid,$dip,$dir){
   $request['vid'] = $vid;
   $request['dip'] = $dip;
   $request['dir'] = $dir;
+  $request['machine'] = $machine;
   $response = $client->send_request($request);
 
   echo "client received response: ".PHP_EOL;
@@ -90,27 +127,28 @@ function doPush($vid,$caseD,$user,$dServDir,$layer){
   deployNew($vid,$user,$dServDir);
 
   //call the function
-  sendtoServer($layer,$caseD,$vid,"N/A","N/A");
+  sendtoServer($layer,$caseD,$vid,"N/A","N/A","n/a");
 }
 
 switch ($caseD)
 {
   case "new":
+    $vid = checkDBVer($layer,$machine);
     doPush($vid,$caseD,$user,$dServDir,$layer);
     break;
   case "deploy":
+    $vid = "";
     //check to make sure you eneter an destination IP
     //this will be used by the deploy server to send the bundle to the right VM
-    if(empty($argv[3]) || empty($argv[4])){
-      echo "Please enter a destination IP".PHP_EOL;
-      echo "Usage: .deplyClient.php "."<Function> <Version ID> <Destination Machine> <Destination Directory>".PHP_EOL;
+    if(empty($argv[2]) || empty($argv[3])){
+      echo "Please enter a destination Machine and Directory".PHP_EOL;
+      echo "Usage: .deplyClient.php "."<Function> <Destination Machine> <Destination Directory> <VersionID>".PHP_EOL;
       exit(0);
     }
     // this is the "DNS" check a flat file and get the IP for that VM
     //if not found exit and tell user why
-    if(isset($argv[3])){
-      $output = shell_exec("cd ../;
-                            cat DNS.conf | grep -i $argv[3] | awk '{ print $1 }'");
+    if(isset($argv[2])){
+      $output = shell_exec("cd ../; cat DNS.conf | grep -i $argv[2] | awk '{ print $1 }'");
       if(isset($output)){
         $string = trim(preg_replace('/\s+/', ' ', $output));
         $destIP = $string;
@@ -119,22 +157,27 @@ switch ($caseD)
         exit(0);
       }
     }
-    $dir= $argv[4];
-    deploy($vid,$destIP,$dir,$layer);
+    if(isset($argv[4])){
+      $vid = $argv[4];
+    }else{
+      $vid = checkLtsVer($layer,$machine);
+    }
+    $dir= $argv[3];
+    deploy($vid,$destIP,$dir,$layer,$machine);
     break;
   case "rollback":
+    $vid = "";
     //check to make sure you eneter an destination IP
     //this will be used by the deploy server to send the bundle to the right VM
-    if(empty($argv[3]) || empty($argv[4])){
-      echo "Please enter a destination IP".PHP_EOL;
-      echo "Usage: .deplyClient.php "."<Function> <Version ID> <Destination IP> <Destination Directory>".PHP_EOL;
+    if(empty($argv[2]) || empty($argv[3])){
+      echo "Please enter a destination Machine and Directory".PHP_EOL;
+      echo "Usage: .deplyClient.php "."<Function> <Destination Machine> <Destination Directory> <VersionID>".PHP_EOL;
       exit(0);
     }
     // this is the "DNS" check a flat file and get the IP for that VM
     //if not found exit and tell user why
-    if(isset($argv[3])){
-      $output = shell_exec("cd ../;
-                            cat DNS.conf | grep -i $argv[3] | awk '{ print $1 }'");
+    if(isset($argv[2])){
+      $output = shell_exec("cd ../; cat DNS.conf | grep -i $argv[2] | awk '{ print $1 }'");
       if(isset($output)){
         $string = trim(preg_replace('/\s+/', ' ', $output));
         $destIP = $string;
@@ -143,13 +186,22 @@ switch ($caseD)
         exit(0);
       }
     }
-    $dir= $argv[4];
-    deploy($vid,$destIP,$dir,$layer);
+    $dir= $argv[3];
+    $vid = checkRoll($layer,$machine);
+    rollback($vid,$destIP,$dir,$layer);
+    break;
+  case "deprecate":
+    if(empty($argv[2])){
+      echo "Please enter a Version ID to deprecate".PHP_EOL;
+      echo "Usage: .deplyClient.php deprecate <Version ID>".PHP_EOL;
+      exit(0);
+    }
+    $vid = $argv[3];
+    sendtoServer($layer,"deprecate",$vid,"n/a","n/a",$machine);
     break;
   default:
     echo "Please enter a valid function".PHP_EOL;
     break;
 }
-
 echo $argv[0]." END".PHP_EOL;
 ?>
